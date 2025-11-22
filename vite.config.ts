eaaf3a45f93d3cc0c34322a88460bdefcbdbc992
nekopills <dudosia585@gmail.com>
2025-11-22 17:11:14 +0300

    try fix build

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,14 @@
     import { defineConfig } from 'vite'
-    import react from '@vitejs/plugin-react'
-    import type { UserConfig } from 'vite';
+    import react from '@vitejs/plugin-react';
 
     export default defineConfig({
       plugins: [react()],
       base: '/lab1-react-vite-gh-pages', 
+      // @ts-ignore
       test: { 
         globals: true, 
         environment: 'jsdom', 
         setupFiles: './src/setupTests.ts', 
         css: true, 
-      } as UserConfig, 
+      }
     });
