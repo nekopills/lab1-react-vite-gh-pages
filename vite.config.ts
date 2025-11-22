9ee256ef9e957aedbbe4a8df89fecad8cb08d7b9
nekopills <dudosia585@gmail.com>
2025-11-22 15:20:53 +0300

    try fix build

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,5 @@
     import { defineConfig } from 'vite'
     import react from '@vitejs/plugin-react'
-    import { UserConfig } from 'vitest/config'; 
 
     export default defineConfig({
       plugins: [react()],
